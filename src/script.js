80ae6f1ab7675e31f08d8f1c7f45f965bc4ef584
Jane-Mar <106934020+Jane-Mar@users.noreply.github.com>
2022-06-30 14:16:47 +0300

    added data&time display

--- src/script.js
+++ src/script.js
@@ -1,3 +1,30 @@
+function formatDate(timeDate) {
+  let data = new Date(timeDate);
+
+  let hour = data.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = data.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let weekDay = days[data.getDay()];
+
+  let year = data.getFullYear();
+  let month = data.getMonth() + 1;
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  let day = data.getDate();
+
+  let currentTime = document.querySelector("#time");
+  currentTime.innerHTML = `${weekDay}, ${hour}:${minutes}`;
+  let currentData = document.querySelector("#data");
+  currentData.innerHTML = `${day}/${month}/${year}`;
+}
+
 function displayWeather(response) {
   console.log(response.data);
   let temperature = document.querySelector("#temperature");
@@ -11,6 +38,9 @@ function displayWeather(response) {
 
   let description = document.querySelector("#description");
   description.innerHTML = response.data.weather[0].description;
+
+  let dateElement = document.querySelector("#time");
+  dateElement = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "ea2c48b1c8a418ed0296eb92935bdf5a";
